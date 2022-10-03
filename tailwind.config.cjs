7a15215806f69f3adab2288daea84ff6e700c146
zachuri <punsalang.zachary@gmail.com>
2022-10-03 04:58:14 -0700

    tailwind.config.cjs: added custom colors for dark/light colors text/bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,12 @@ module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		colors: {
+			"dark-color-bg": "#111111",
+			"dark-color-text": "EEEEEE",
+			"light-color-bg": "#F4F4F4",
+			"light-color-text": "111111",
+		},
 	},
 	plugins: [],
 };
