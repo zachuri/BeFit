47712e2fe2e6cc1b72330a3adaef10c307865530
zachuri <punsalang.zachary@gmail.com>
2022-10-03 05:00:01 -0700

    navbar: general setup left/middle/right

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from 'next-themes'
-import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   // Color Theme
@@ -12,9 +12,23 @@ const Navbar = () => {
 
   return (
     <>
-      <div>
-        <div>Navbar</div>
-        <button onClick={handleTheme} className="border border-black rounded-lg">Toggle Theme</button>
+      {/* Container for Navbar */}
+      <div className='top-0 fixed w-full h-10 md:h-15 z-[100] bg-opacity-20 backdrop-blur-lg drop-shadow-lg'>
+        <div className='flex justify-between items-center w-full h-full px-10'>
+          {/* Left */}
+          <div>
+            <Link href='/'>BeFit</Link>
+          </div>
+
+          {/* Middle */}
+          <div>Middle</div>
+
+          {/* Right */}
+          <div>
+            <button onClick={handleTheme} className='border border-black dark:border-white rounded-lg'>Toggle Theme</button>
+          </div>
+
+        </div>
       </div>
     </>
   )
