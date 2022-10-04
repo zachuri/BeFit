17064a131b0d707e4816da8a55b7efb9f955f0ef
zachuri <punsalang.zachary@gmail.com>
2022-10-03 21:05:16 -0700

    navbar: added border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,75 +1,74 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
-import { useTheme } from 'next-themes'
-import Link from 'next/link'
-import { WiMoonAltThirdQuarter } from 'react-icons/wi'
-import { CgProfile } from 'react-icons/cg'
-import { AccountMenu, NavigateMenu } from './NavbarMenus'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import { WiMoonAltThirdQuarter } from 'react-icons/wi';
+import { CgProfile } from 'react-icons/cg';
+import { AccountMenu, NavigateMenu } from './NavbarMenus';
 
 const Navbar = () => {
   // Color Theme
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   // Handle Color Theme
   function handleTheme() {
-    theme === "dark" ? setTheme("light") : setTheme("dark")
-    console.log("Theme: " + theme);
+    theme === 'dark' ? setTheme('light') : setTheme('dark');
+    console.log('Theme: ' + theme);
   }
 
   const { data: session } = useSession();
 
   console.log(session);
 
-
   return (
     <>
       {/* Container for Navbar */}
-      <div className='top-0 fixed w-full h-10 md:h-20 z-[100] bg-opacity-20 backdrop-blur-lg drop-shadow-lg border-b-2'>
-        <div className='flex justify-between items-center w-full h-full px-10'>
+      <div className="top-0 fixed w-full h-10 md:h-20 z-[100] bg-opacity-20 backdrop-blur-lg drop-shadow-lg border-b-2">
+        <div className="flex justify-between items-center w-full h-full px-10">
           {/* Left */}
           <div>
-            <Link href='/'>BeFit</Link>
+            <Link href="/">BeFit</Link>
           </div>
 
           {/* Middle */}
           {/* <div>Middle</div> */}
 
           {/* Right */}
-          <div className='flex flex-cols'>
+          <div className="flex flex-cols">
             {/* ThreeDot for Navigating to pages */}
             <NavigateMenu />
 
             {/* Button Theme Toggle */}
-            <button onClick={handleTheme} className='p-2 mr-2'>
+            <button onClick={handleTheme} className="p-2 mr-2">
               <WiMoonAltThirdQuarter size={20} />
             </button>
 
             {/* Log in/out */}
-            {!session ?
-              (
-                <div className='flex items-center justify-center'>
-                  {/* Log In */}
-                  <button onClick={() => signIn()} className='mr-1 p-1'>Sign In</button>
+            {!session ? (
+              <div className="flex items-center justify-center">
+                {/* Log In */}
+                <button onClick={() => signIn()} className="mr-1 p-1">
+                  Sign In
+                </button>
 
-                  {/* Empty Avatar */}
-                  <CgProfile size={25} />
-                </div>
-              )
-              : (
-                <div className='flex flex-cols'>
-                  {/* Log out */}
-                  <button onClick={() => signOut()} className='mr-1 p-1'>Sign Out</button>
+                {/* Empty Avatar */}
+                <CgProfile size={25} />
+              </div>
+            ) : (
+              <div className="flex flex-cols">
+                {/* Log out */}
+                <button onClick={() => signOut()} className="mr-1 p-1">
+                  Sign Out
+                </button>
 
-                  {/* Avatar Image */}
-                  <AccountMenu session={session} />
-
-                </div>
-              )
-            }
+                {/* Avatar Image */}
+                <AccountMenu session={session} />
+              </div>
+            )}
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
