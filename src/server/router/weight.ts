75712c33895b69c232bd52fc9092ed8895a05bac
zachuri <punsalang.zachary@gmail.com>
2022-10-04 23:04:21 -0700

    weight.router: added middleware to protect the user weight data

--- src/server/router/weight.ts
+++ src/server/router/weight.ts
@@ -2,8 +2,17 @@ import { createRouter } from './context';
 // import { z } from "zod";
 import { createWeightSchema } from '../../schema/weight.schema';
 import * as trpc from '@trpc/server';
+import { TRPCError } from '@trpc/server';
 
 export const weightRouter = createRouter()
+  .middleware(async ({ ctx, next }) => {
+    // Any queries or mutations after this middleware will
+    // raise an error unless there is a current session
+    if (!ctx.session) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next();
+  })
   .mutation('create-weight', {
     input: createWeightSchema,
     async resolve({ ctx, input }) {
