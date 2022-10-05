8ed317d4fb97d8bcf13927a127b74a0f152cc5f7
zachuri <punsalang.zachary@gmail.com>
2022-10-05 01:09:58 -0700

    item: split date and time/ added lbs

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -10,9 +10,17 @@ const Item: React.FC<Props> = ({ date, weight, description }) => {
   return (
     <>
       <div className="rounded border border-black dark:border-white p-4">
-        <div>Weight: {weight}</div>
-        <div className="text-xs">Date: {date}</div>
+        <div>
+          Weight: {weight} <span className="text-xs">lbs</span>
+        </div>
         <div className="text-xs">Description: {description}</div>
+        <div className="text-xs">
+          {/* Only get the date */}
+          Date: {date.split(' ').slice(0, 4).join(' ')}
+        </div>
+        <div className="text-xs">
+          Time: {date.split(' ').slice(-2).join(' ')}
+        </div>
       </div>
     </>
   );
