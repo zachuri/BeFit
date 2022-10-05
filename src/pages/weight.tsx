203b7aa36c1a80e8138a932915cb2aab9af193e9
zachuri <punsalang.zachary@gmail.com>
2022-10-05 01:03:36 -0700

    weightPage: added float parse

--- src/pages/weight.tsx
+++ src/pages/weight.tsx
@@ -62,11 +62,13 @@ const Weight: React.FC = () => {
                   </label>
                   <input
                     type="number"
+                    step="0.1"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="180"
+                    placeholder="180.4"
                     required
+                    // {...register('weightTotal')}
                     {...register('weightTotal', {
-                      setValueAs: v => parseInt(v)
+                      setValueAs: v => parseFloat(v)
                     })}
                   />
                 </div>
