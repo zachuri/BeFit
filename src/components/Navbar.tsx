9ac15eb183e4cff1ef80776092bf0ffa27a4b985
zachuri <punsalang.zachary@gmail.com>
2022-10-06 22:51:52 -0700

    navbar: replaced react icons with hero icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,6 @@
 import { signIn, useSession } from 'next-auth/react';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
-import { WiMoonAltThirdQuarter } from 'react-icons/wi';
-import { CgProfile } from 'react-icons/cg';
 import { AccountMenu, NavigateMenu } from './NavbarMenus';
 
 const Navbar = () => {
@@ -39,7 +37,20 @@ const Navbar = () => {
           <div className="flex flex-cols">
             {/* Button Theme Toggle */}
             <button onClick={handleTheme} className="p-2">
-              <WiMoonAltThirdQuarter size={20} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                />
+              </svg>
             </button>
 
             {/* Log in/out */}
@@ -61,7 +72,20 @@ const Navbar = () => {
                 </button>
 
                 {/* Empty Avatar */}
-                <CgProfile size={30} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
               </div>
             )}
           </div>
--- src/components/NavbarMenus.tsx
+++ src/components/NavbarMenus.tsx
@@ -3,7 +3,6 @@ import { Menu, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import router from 'next/router';
-import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const links = [
   { href: '/', name: 'Home' },
@@ -25,7 +24,20 @@ export const NavigateMenu: React.FC = () => {
           role="navigation"
           aria-label="hamburger menu to navigate to pages"
         >
-          <BsThreeDotsVertical size={15} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+            />
+          </svg>
         </Menu.Button>
 
         <Transition
