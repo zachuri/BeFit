f36192501493eece94d4ca553fe4ac28065a12ce
zachuri <punsalang.zachary@gmail.com>
2022-10-11 22:35:52 -0700

    router: added workouts

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,12 +5,14 @@ import superjson from 'superjson';
 import { exampleRouter } from './example';
 import { protectedExampleRouter } from './protected-example-router';
 import { weightRouter } from './weight';
+import { workoutRouter } from './workout';
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('example.', exampleRouter)
   .merge('auth.', protectedExampleRouter)
-  .merge('weights.', weightRouter);
+  .merge('weights.', weightRouter)
+  .merge('workouts.', workoutRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
