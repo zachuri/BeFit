7a4e81121f9b81b9cb6b333f802e0f60492a5937
zachuri <punsalang.zachary@gmail.com>
2022-10-15 15:32:47 -0700

    404: created a 404 page to return home

---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import React from 'react';
+import { MainLayoutHeightScreen } from '../components/layouts/Main';
+
+const NotFound = () => {
+  return (
+    <MainLayoutHeightScreen>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl">Not Found</h1>
+        <p className="mt-5">The page you&apos;re looking for was not found</p>
+
+        <hr className="mt-5 border w-full" />
+
+        <div className="mt-5">
+          <Link href="/">
+            <button className="rounded p-1 border border-black dark:border-white">
+              Return Home
+            </button>
+          </Link>
+        </div>
+      </div>
+    </MainLayoutHeightScreen>
+  );
+};
+
+export default NotFound;
