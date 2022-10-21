7fea6c955ba0bb78912c9be2b12aca32ad5460b2
zachuri <punsalang.zachary@gmail.com>
2022-10-21 00:48:20 -0700

    trackerItem: added table body component to display items as row for table

--- src/components/ExerciseItem/TrackerItems.tsx
+++ src/components/ExerciseItem/TrackerItems.tsx
@@ -9,11 +9,25 @@ interface Props {
 
 const TrackerItems: React.FC<Props> = ({ id, set, rep, weight }) => {
   return (
-    <div className="my-5" key={id}>
-      <p>Set: {set}</p>
-      <p>Rep: {rep}</p>
-      <p>Weight: {weight}</p>
-    </div>
+    <tbody>
+      <tr className="text-center border-t border-black dark:border-white dark:border-t overflow-auto">
+        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          {set}
+        </td>
+        <td>{rep}</td>
+        <td>{weight}</td>
+        <td>
+          <div className="mx-2 flex flex-row gap-2">
+            <button className="text-xs rounded p-1 border border-black dark:border-white dark:hover:border-blue-500 hover:border-blue-500 transition">
+              Edit
+            </button>
+            <button className="text-xs rounded p-1 border border-black dark:border-white dark:hover:border-[red] hover:border-[red]">
+              Remove
+            </button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
   );
 };
 
