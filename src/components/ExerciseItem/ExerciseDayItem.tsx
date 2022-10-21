a471d037c79812f391c97e7f689b28343ca33057
zachuri <punsalang.zachary@gmail.com>
2022-10-21 04:09:24 -0700

    exerciseDayItem: added remove confirmation

--- src/components/ExerciseItem/ExerciseDayItem.tsx
+++ src/components/ExerciseItem/ExerciseDayItem.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import React, { Fragment, useState } from 'react';
 import { trpc } from '../../utils/trpc';
 import ExerciseItemTracker from './ExerciseTrackerItem';
 
@@ -36,6 +37,10 @@ const ExerciseDayItem: React.FC<Props> = ({ exerciseId, id, date }) => {
     }
   });
 
+  const [isOpenRemove, setIsOpenRemove] = useState(false);
+  const [inputRemove, setInputRemove] = useState('');
+  const [errorRemoveInput, setErrorRemoveInput] = useState(false);
+
   return (
     <div className="mb-5 w-full rounded border border-white">
       <div className="m-5 flex flex-row items-center justify-between">
@@ -49,7 +54,9 @@ const ExerciseDayItem: React.FC<Props> = ({ exerciseId, id, date }) => {
         </div>
         <div className="flex items-center justify-center">
           <button
-            onClick={() => mutateRemove({ id: id })}
+            onClick={() => {
+              setIsOpenRemove(true);
+            }}
             className="rounded border border-white"
           >
             <svg
@@ -71,6 +78,105 @@ const ExerciseDayItem: React.FC<Props> = ({ exerciseId, id, date }) => {
       </div>
 
       <ExerciseItemTracker exerciseDayId={id} />
+
+      {/*  Modal for Remove */}
+      <Transition appear show={isOpenRemove} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => {
+            setIsOpenRemove(false);
+          }}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white border border-black  dark:bg-black dark:border dark:border-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900 dark:text-white"
+                  >
+                    Are you sure you want to remove?
+                  </Dialog.Title>
+                  <h3 className="text-center mt-4 text-xs dark:text-gray-100">
+                    Please type{' '}
+                    <span className="text-gray-500">&quot;{date}&quot;</span> to{' '}
+                    <span className="text-red-500">delete forever</span>
+                  </h3>
+                  <div className="flex justify-center item-center my-4">
+                    <input
+                      type="text"
+                      placeholder={date}
+                      className="rounded p-2 mr-2 border border-black dark:border-white"
+                      onChange={e => {
+                        setInputRemove(e.target.value);
+                      }}
+                    />
+                    <button
+                      onClick={() => {
+                        if (inputRemove === date) {
+                          mutateRemove({ id: id });
+                          setIsOpenRemove(false);
+                        }
+                        setErrorRemoveInput(true);
+                      }}
+                      className="border border-black dark:border-white hover:bg-[red] hover:border-white hover:dark:border-[red] p-2 rounded"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                  <h2 className="text-center text-xs text-red-500">
+                    WARNING: All your sets and reps will be wiped
+                  </h2>
+
+                  {errorRemoveInput && (
+                    <div>
+                      Wrong name! Please input
+                      <span className="text-gray-500">
+                        &quot;{date}&quot;
+                      </span>{' '}
+                      remove
+                    </div>
+                  )}
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => {
+                        setIsOpenRemove(false);
+                        setErrorRemoveInput(false);
+                      }}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 };
