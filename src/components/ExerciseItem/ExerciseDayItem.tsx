6c58371a9d47dacd98b0f43629b2c6e25a09aa53
zachuri <punsalang.zachary@gmail.com>
2022-10-21 00:30:09 -0700

    exerciseDayItem: fixed layout/ only showed date removed id/ added remove x icon

--- src/components/ExerciseItem/ExerciseDayItem.tsx
+++ src/components/ExerciseItem/ExerciseDayItem.tsx
@@ -37,22 +37,35 @@ const ExerciseDayItem: React.FC<Props> = ({ exerciseId, id, date }) => {
   });
 
   return (
-    <div className="my-5 rounded border border-white">
-      <div className="flex flex-row justify-between">
+    <div className="w-full my-5 rounded border border-white">
+      <div className="flex flex-row items-center justify-between mt-2">
         {errorRemove && <div>{errorRemove.message}</div>}
 
         {/* Will Display the current Date / able to remove */}
         <div className="">
-          <div className="p-2">Exercise ID: {exerciseId}</div>
-          <div className="p-2">ExerciseDay ID: {id}</div>
-          <div className="text-left p-2">Date: {date}</div>
+          {/* <div className="p-2">Exercise ID: {exerciseId}</div>
+          <div className="p-2">ExerciseDay ID: {id}</div> */}
+          <div className="text-2xl text-left p-2">Date: {date}</div>
         </div>
         <div className="flex items-center justify-center">
           <button
             onClick={() => mutateRemove({ id: id })}
-            className="rounded p-1 border borer-white"
+            className="rounded border border-white mr-4"
           >
-            remove
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
           </button>
         </div>
       </div>
